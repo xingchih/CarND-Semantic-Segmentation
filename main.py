f0449c3d73cab8e9379d8e849bb8a4f5c58bfa52
hxc <he.xingchi@gmail.com>
2017-10-29 17:22:08 -0700

    correct missing

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     labels = tf.reshape(correct_label, (-1, num_classes))
 
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
     
     # define adam optimizer
     train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
